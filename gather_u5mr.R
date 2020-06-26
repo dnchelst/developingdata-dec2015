34c598b963a0250f61b2057c678494885be2077b
dnchelst <dchelst@cpsm.us>
2020-06-26 11:55:23 -0400

    updated gather_u5mr file, the RData file and a new world-only csv file.

--- gather_u5mr.R
+++ gather_u5mr.R
@@ -2,6 +2,7 @@
 library(choroplethrMaps)
 library(WDI)
 library(tidyverse)
+library(magrittr)
 
 
 
@@ -12,18 +13,20 @@ country.regions <-  country.regions %>%
                            TRUE ~ iso2c))
 mortality <- WDI(country= "all", #country.regions$iso2c, 
                  indicator="SH.DYN.MORT", start=1990, 
-                 extra=FALSE, cache=NULL)
-mortality2 <- merge(mortality, country.regions) %>% 
-  filter(!is.na(SH.DYN.MORT)) %>% 
+                 extra=FALSE, cache=NULL) %>% 
   rename(value=SH.DYN.MORT) %>% 
+  filter(!is.na(value))
+mortality.world <- mortality %>% filter(iso2c=="1W") 
+mortality2 <- merge(mortality, country.regions) %>% 
   group_by(year) %>% 
   mutate(rank = rank(value)) %>% 
   ungroup
 write_csv(mortality2, "u5mr.csv")
+write_csv(mortality.world, "u5mr-world.csv")
 
 region.choices <- mortality2 %>% 
   distinct(region) %>% 
   arrange(region) %$% 
   region
 
-save(mortality2, region.choices, file="u5mr.RData")
+save(mortality2, mortality.world, region.choices, file="u5mr.RData")
